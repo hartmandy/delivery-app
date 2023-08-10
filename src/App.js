97446b1d461cacd85226654f99539fc7c7681dcc
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-08-09 22:00:11 -0400

    added cart and modals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
